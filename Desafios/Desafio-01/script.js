45c372ed0afd947677aae6595c92cff33993d7f4
Wesley Dutra Paiz <wesleydtrp19@gmail.com>
2025-06-28 13:14:33 -0300

    Capítulo 01 + desafio 01

---
+++ Desafios/Desafio-01/script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: 'Wesley Dutra',
+        age: 23,
+        img: 'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'
+    },
+    methods: {
+    }
+})
