f7e728f9eccb9a4cb8047b64acd7c12464818a25
Wesley Dutra Paiz <wesleydtrp19@gmail.com>
2025-08-22 19:43:18 -0300

    Capítulo 05 + desafio 05

---
+++ Desafios/Desafio-05/script.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        see: true,
+        wickedSongs: ['No Good Deed', 'What Is This Feeling', 'I´m Not That Girl', 'Defying Gravity'],
+        book: {
+            title: 'Lord of  the rings',
+            author:  'J.R.R Tolkiens',
+            volume: '3'
+        },
+        student: {
+            id: 1,
+            name: 'Felipe',
+            grade: [10.0, 8.5, 7.2, 3.4]
+        }
+    },
+    methods: {
+
+    }
+})
